13cd821bd0a9719146f87e989d61180a378e0805
Adam <adamjonez14@gmail.comglobal github.user adam jones$ git config --global user.name adam>
2012-11-11 18:50:31 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arhw::Application.config.secret_token = 'b0b1bf92fbcba768bd6cf99a61698fb2dcd4d2073e984e515ee48e4488997bf8c634881184ecb582abbd3d679faef7c705c48fa1fff4d5d7ef5a91054b0c9aff'
